f4a12dd170dd669b11ac1cd5bdfcc7f8e6751b99
Md Zahid Hasan <hasanmd1@fit.cvut.cz>
2024-09-12 22:39:00 +0200

    Finishing the EDA part

--- Credit_Card_Fraud_Detection/src/__init__.py
+++
@@ -1,1 +0,0 @@
-from data_preprocessing import load_data, clean_data, scale_features
--- Credit_Card_Fraud_Detection/src/data_preprocessing.py
+++ Credit_Card_Fraud_Detection/src/data_preprocessing.py
@@ -17,24 +17,39 @@ def clean_data(df):
 
 def scale_features(df):
     scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(df.select_dtypes(np.number))
-    return pd.DataFrame(scaled_features, columns=df.select_dtypes(np.number).columns)
+    return scaler.fit_transform(df.select_dtypes(np.number))
 
 
-def under_sampling(df):
+# return pd.DataFrame(scaled_features, columns=df.select_dtypes(np.number).columns)
+
+
+def under_sampling(df, target):
     df = df.sample(frac=1, random_state=42)
-    df_0 = df[df['Class'] == 0]
-    df_1 = df[df['Class'] == 1]
-    df_1_under = df_1.sample(len(df_0), random_state=42)
-    new_df = pd.concat([df_0, df_1_under], axis=0)
+    df_0 = df[df[target] == 0]
+    df_1 = df[df[target] == 1]
+    df_0_under = df_0.sample(n=len(df_1), random_state=42)
+    new_df = pd.concat([df_1, df_0_under], axis=0)
 
     # basically shuffling data, fixing indexing and returning dataframe
     return new_df.sample(frac=1, random_state=42).reset_index(drop=True)
 
 
-def over_sampling(df):
-    df_0 = df[df['Class'] == 0]
-    df_1 = df[df['Class'] == 1]
-    df_0_over = df_0.sample(len(df_1), replace=True)
-    df = pd.concat([df_1, df_0_over], axis=0)
-    return df
+def over_sampling(df, target_name):
+    # first separate features and target
+    features = df.drop(columns=target_name, axis=1)
+    target = df[target_name]
+
+    # apply SMOTE
+    smote = SMOTE(random_state=42, k_neighbors=10)
+    # fit for all fraud non-fraud number and apply the transform
+    features_resampled, target_resampled = smote.fit_resample(features, target)
+
+    features_resampled_df = pd.DataFrame(features_resampled, columns=features.columns)
+    target_resampled_df = pd.DataFrame(target_resampled, columns=[target_name])
+
+    # recombine features and target and return
+    return pd.concat([features_resampled_df, target_resampled_df], axis=1)
+
+# if __name__ == '__main__':
+#     df = load_data('../dataset/raw/creditcard.csv')
+#     over_sampling(df)
